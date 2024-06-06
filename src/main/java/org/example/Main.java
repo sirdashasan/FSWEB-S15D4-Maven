6e1510799a4b0f029279a8be7a84e56c658e6c40
sirdashasan <sirdashasan@hotmail.com>
2024-06-06 20:14:26 +0300

    Görevler tamamlandı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,44 @@
 package org.example;
 
 
+import java.util.LinkedList;
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(checkForPalindrome("Was it a car or a cat i saw"));
     }
+
+    public static boolean checkForPalindrome(String text){
+
+        String cleanedStr = text.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(Locale.ENGLISH);
+        LinkedList<Character> charList = new LinkedList<>();
+
+        for(char c: cleanedStr.toCharArray()){
+            charList.add(c);
+        }
+
+        while(charList.size()>1){
+            if(!charList.pollFirst().equals(charList.pollLast())){
+                return false;
+            }
+        }
+        return true;
+     }
+
+     public static String convertDecimalToBinary(int decimal){
+        LinkedList<Integer> binaryList = new LinkedList<>();
+        while (decimal>0){
+            int remainder = decimal % 2;
+            binaryList.addFirst(remainder);
+            decimal = decimal/2;
+        }
+        StringBuilder binaryString = new StringBuilder();
+        for(int digit: binaryList){
+            binaryString.append(digit);
+        }
+        return binaryString.toString();
+     }
 
 }
